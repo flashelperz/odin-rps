0dea827c4835b2196b4f067ca3ef6986dcf1b0c5
flashelperz <steriamfogue@gmail.com>
2024-10-21 19:53:42 +0100

    feat: computer random choice

--- js/app.js
+++ js/app.js
@@ -22,6 +22,44 @@ function showWelcome() {
     console.log(line1 + message + line2);
 }
 
+/**
+ * Convert a picked number to it's corresponding 
+ * in-game value
+ * 
+ * @param {*} pickedNumber 
+ * @returns 
+ */
+function correspondingGameByNumber(pickedNumber) {
+    const parsed = +pickedNumber;
+    if (isNaN(parsed) || parsed < 0 || parsed > 2) {
+        throw new Error("The picked number can only be 0, 1 or 2");
+    }
+
+    return parsed === 0
+        ? 'rock'
+        : parsed === 1
+            ? 'paper'
+            : 'scissors';
+}
+
+/**
+ * This method returned the computer choice
+ * randomly, the choices are:
+ * `0`: for `rock`
+ * `1`: for `paper`
+ * `2`: for `scissors`
+ * 
+ * The method used there for the random choice is Math.random
+ * 
+ * @returns
+ */
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    console.log("Computer choice", choice);
+
+    return choice;
+}
+
 
 /**
  * Main Method to start the game
